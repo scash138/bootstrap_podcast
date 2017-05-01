0710be40c4fbed3d30d859f97c397d06884bd91f
Sterling Cash <sterlingcash138@gmail.com>
2017-04-30 23:42:52 -0500

    added pagination

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -40,12 +40,44 @@ $(function(){
     //get ajax json podcast guests
     var podcastData = $.each(data.podcasts, function(index, el) {
     });
+    //page-count buttons
     var pages = Math.ceil(podcastData.length/9);
-    if(pages > 1) {
-      for (var i = 0; i < pages; i++) {
-        $('.page-count').append('<button type="button" class="btn-group btn-default" value="' + (i+1) + '">' + (i+1) + '</button>');
-      }
-    }//end get ajax json podcast guests
+    //pagination for ajax podcast guests
+    $('#podcast-pagination-group').twbsPagination({
+        totalPages: pages,
+        visiblePages: 4,
+        onPageClick: function (event, page) {
+
+          var ubound = (page * 9);
+          var lbound = ((page * 9) - 9);
+
+          $.each($('#podcast .guest'),function() {
+            if(podcastData[lbound] == null) {
+              $('h3', this).text('');
+              $('h4', this).text('');
+              $('p', this).text('');
+              $('.photo img', this).removeClass('img-circle');
+              $('.photo img', this).removeAttr('src');
+              $('.photo img', this).removeAttr('alt');
+              $('.photo a', this).hide();
+              lbound +=1;
+            } else {
+              $('h3', this).text(podcastData[lbound].guest);
+              $('h4', this).text('Episode #' + podcastData[lbound].episode + ' - ' + podcastData[lbound].date);
+              $('p', this).text(podcastData[lbound].description);
+              $('.photo img', this).addClass('img-circle');
+              $('.photo img', this).attr('src',podcastData[lbound].image);
+              $('.photo img', this).attr('alt',podcastData[lbound].alt);
+              $('.photo a', this).attr('href',podcastData[lbound].audiofile);
+              $('.photo a', this).show();
+              lbound +=1;
+            }
+
+          });
+
+          showMore(); //run show more to limit characters in description
+        }
+    });
 
     //populate elements with podcast guests
     $.each($('#podcast .guest'),function(index, el) {
@@ -59,38 +91,6 @@ $(function(){
     });
 
     showMore(); //run show more to limit characters in description
-
-    //onclick to replace elements with new guests
-    $('.next-page button').click(function(){
-      var ubound = ($(this).val() * 9);
-      var lbound = (($(this).val() * 9) - 9);
-
-      $.each($('#podcast .guest'),function() {
-        if(podcastData[lbound] == null) {
-          $('h3', this).text('');
-          $('h4', this).text('');
-          $('p', this).text('');
-          $('.photo img', this).removeClass('img-circle');
-          $('.photo img', this).removeAttr('src');
-          $('.photo img', this).removeAttr('alt');
-          $('.photo a', this).hide();
-          lbound +=1;
-        } else {
-          $('h3', this).text(podcastData[lbound].guest);
-          $('h4', this).text('Episode #' + podcastData[lbound].episode + ' - ' + podcastData[lbound].date);
-          $('p', this).text(podcastData[lbound].description);
-          $('.photo img', this).addClass('img-circle');
-          $('.photo img', this).attr('src',podcastData[lbound].image);
-          $('.photo img', this).attr('alt',podcastData[lbound].alt);
-          $('.photo a', this).attr('href',podcastData[lbound].audiofile);
-          $('.photo a', this).show();
-          lbound +=1;
-        }
-
-      });
-
-      showMore(); //run show more to limit characters in description
-    });
 
     //search function
     $('.searchpodcast').click(function(){
@@ -106,38 +106,7 @@ $(function(){
 
 })
   .done(function(data) {
-      // var showChar = 100;  // How many characters are shown by default
-      // var ellipsestext = "...";
-      // var moretext = "Show more >>";
-      // var lesstext = "<< Show less";
-      //
-      //
-      // $('.guest-description').each(function() {
-      //   var content = $(this).html();
-      //
-      //   if(content.length > showChar) {
-      //
-      //       var c = content.substr(0, showChar);
-      //       var h = content.substr(showChar, content.length - showChar);
-      //
-      //       var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
-      //
-      //       $(this).html(html);
-      //   }
-      // });
-      //
-      // $(".morelink").click(function(){
-      //   if($(this).hasClass("less")) {
-      //       $(this).removeClass("less");
-      //       $(this).html(moretext);
-      //   } else {
-      //       $(this).addClass("less");
-      //       $(this).html(lesstext);
-      //   }
-      //   $(this).parent().prev().toggle();
-      //   $(this).prev().toggle();
-      //   return false;
-      // });
+
   })
   .fail(function() {
 
@@ -175,6 +144,15 @@ $(function(){
   });
 });
 
+/*
+ * jQuery Bootstrap Pagination v1.4.1
+ * https://github.com/esimakin/twbs-pagination
+ *
+ * Copyright 2014-2016, Eugene Simakin <john-24@list.ru>
+ * Released under Apache-2.0 license
+ * http://apache.org/licenses/LICENSE-2.0.html
+ */
+!function(a,b,c,d){"use strict";var e=a.fn.twbsPagination,f=function(b,c){if(this.$element=a(b),this.options=a.extend({},a.fn.twbsPagination.defaults,c),this.options.startPage<1||this.options.startPage>this.options.totalPages)throw new Error("Start page option is incorrect");if(this.options.totalPages=parseInt(this.options.totalPages),isNaN(this.options.totalPages))throw new Error("Total pages option is not correct!");if(this.options.visiblePages=parseInt(this.options.visiblePages),isNaN(this.options.visiblePages))throw new Error("Visible pages option is not correct!");if(this.options.onPageClick instanceof Function&&this.$element.first().on("page",this.options.onPageClick),this.options.hideOnlyOnePage&&1==this.options.totalPages)return this.$element.trigger("page",1),this;this.options.totalPages<this.options.visiblePages&&(this.options.visiblePages=this.options.totalPages),this.options.href&&(this.options.startPage=this.getPageFromQueryString(),this.options.startPage||(this.options.startPage=1));var d="function"==typeof this.$element.prop?this.$element.prop("tagName"):this.$element.attr("tagName");return"UL"===d?this.$listContainer=this.$element:this.$listContainer=a("<ul></ul>"),this.$listContainer.addClass(this.options.paginationClass),"UL"!==d&&this.$element.append(this.$listContainer),this.options.initiateStartPageClick?this.show(this.options.startPage):(this.currentPage=this.options.startPage,this.render(this.getPages(this.options.startPage)),this.setupEvents()),this};f.prototype={constructor:f,destroy:function(){return this.$element.empty(),this.$element.removeData("twbs-pagination"),this.$element.off("page"),this},show:function(a){if(a<1||a>this.options.totalPages)throw new Error("Page is incorrect.");return this.currentPage=a,this.render(this.getPages(a)),this.setupEvents(),this.$element.trigger("page",a),this},enable:function(){this.show(this.currentPage)},disable:function(){var b=this;this.$listContainer.off("click").on("click","li",function(a){a.preventDefault()}),this.$listContainer.children().each(function(){var c=a(this);c.hasClass(b.options.activeClass)||a(this).addClass(b.options.disabledClass)})},buildListItems:function(a){var b=[];if(this.options.first&&b.push(this.buildItem("first",1)),this.options.prev){var c=a.currentPage>1?a.currentPage-1:this.options.loop?this.options.totalPages:1;b.push(this.buildItem("prev",c))}for(var d=0;d<a.numeric.length;d++)b.push(this.buildItem("page",a.numeric[d]));if(this.options.next){var e=a.currentPage<this.options.totalPages?a.currentPage+1:this.options.loop?1:this.options.totalPages;b.push(this.buildItem("next",e))}return this.options.last&&b.push(this.buildItem("last",this.options.totalPages)),b},buildItem:function(b,c){var d=a("<li></li>"),e=a("<a></a>"),f=this.options[b]?this.makeText(this.options[b],c):c;return d.addClass(this.options[b+"Class"]),d.data("page",c),d.data("page-type",b),d.append(e.attr("href",this.makeHref(c)).addClass(this.options.anchorClass).html(f)),d},getPages:function(a){var b=[],c=Math.floor(this.options.visiblePages/2),d=a-c+1-this.options.visiblePages%2,e=a+c;d<=0&&(d=1,e=this.options.visiblePages),e>this.options.totalPages&&(d=this.options.totalPages-this.options.visiblePages+1,e=this.options.totalPages);for(var f=d;f<=e;)b.push(f),f++;return{currentPage:a,numeric:b}},render:function(b){var c=this;this.$listContainer.children().remove();var d=this.buildListItems(b);a.each(d,function(a,b){c.$listContainer.append(b)}),this.$listContainer.children().each(function(){var d=a(this),e=d.data("page-type");switch(e){case"page":d.data("page")===b.currentPage&&d.addClass(c.options.activeClass);break;case"first":d.toggleClass(c.options.disabledClass,1===b.currentPage);break;case"last":d.toggleClass(c.options.disabledClass,b.currentPage===c.options.totalPages);break;case"prev":d.toggleClass(c.options.disabledClass,!c.options.loop&&1===b.currentPage);break;case"next":d.toggleClass(c.options.disabledClass,!c.options.loop&&b.currentPage===c.options.totalPages)}})},setupEvents:function(){var b=this;this.$listContainer.off("click").on("click","li",function(c){var d=a(this);return!d.hasClass(b.options.disabledClass)&&!d.hasClass(b.options.activeClass)&&(!b.options.href&&c.preventDefault(),void b.show(parseInt(d.data("page"))))})},makeHref:function(a){return this.options.href?this.generateQueryString(a):"#"},makeText:function(a,b){return a.replace(this.options.pageVariable,b).replace(this.options.totalPagesVariable,this.options.totalPages)},getPageFromQueryString:function(a){var b=this.getSearchString(a),c=new RegExp(this.options.pageVariable+"(=([^&#]*)|&|#|$)"),d=c.exec(b);return d&&d[2]?(d=decodeURIComponent(d[2]),d=parseInt(d),isNaN(d)?null:d):null},generateQueryString:function(a,b){var c=this.getSearchString(b),d=new RegExp(this.options.pageVariable+"=*[^&#]*");return c?"?"+c.replace(d,this.options.pageVariable+"="+a):""},getSearchString:function(a){var c=a||b.location.search;return""===c?null:(0===c.indexOf("?")&&(c=c.substr(1)),c)},getCurrentPage:function(){return this.currentPage}},a.fn.twbsPagination=function(b){var c,e=Array.prototype.slice.call(arguments,1),g=a(this),h=g.data("twbs-pagination"),i="object"==typeof b?b:{};return h||g.data("twbs-pagination",h=new f(this,i)),"string"==typeof b&&(c=h[b].apply(h,e)),c===d?g:c},a.fn.twbsPagination.defaults={totalPages:1,startPage:1,visiblePages:5,initiateStartPageClick:!0,hideOnlyOnePage:!1,href:!1,pageVariable:"{{page}}",totalPagesVariable:"{{total_pages}}",page:null,first:"First",prev:"Previous",next:"Next",last:"Last",loop:!1,onPageClick:null,paginationClass:"pagination",nextClass:"page-item next",prevClass:"page-item prev",lastClass:"page-item last",firstClass:"page-item first",pageClass:"page-item",activeClass:"active",disabledClass:"disabled",anchorClass:"page-link"},a.fn.twbsPagination.Constructor=f,a.fn.twbsPagination.noConflict=function(){return a.fn.twbsPagination=e,this},a.fn.twbsPagination.version="1.4.1"}(window.jQuery,window,document);
 $(function(){
   'use strict';
 
--- components/js/ajax.js
+++ components/js/ajax.js
@@ -39,12 +39,44 @@ $(function(){
     //get ajax json podcast guests
     var podcastData = $.each(data.podcasts, function(index, el) {
     });
+    //page-count buttons
     var pages = Math.ceil(podcastData.length/9);
-    if(pages > 1) {
-      for (var i = 0; i < pages; i++) {
-        $('.page-count').append('<button type="button" class="btn-group btn-default" value="' + (i+1) + '">' + (i+1) + '</button>');
-      }
-    }//end get ajax json podcast guests
+    //pagination for ajax podcast guests
+    $('#podcast-pagination-group').twbsPagination({
+        totalPages: pages,
+        visiblePages: 4,
+        onPageClick: function (event, page) {
+
+          var ubound = (page * 9);
+          var lbound = ((page * 9) - 9);
+
+          $.each($('#podcast .guest'),function() {
+            if(podcastData[lbound] == null) {
+              $('h3', this).text('');
+              $('h4', this).text('');
+              $('p', this).text('');
+              $('.photo img', this).removeClass('img-circle');
+              $('.photo img', this).removeAttr('src');
+              $('.photo img', this).removeAttr('alt');
+              $('.photo a', this).hide();
+              lbound +=1;
+            } else {
+              $('h3', this).text(podcastData[lbound].guest);
+              $('h4', this).text('Episode #' + podcastData[lbound].episode + ' - ' + podcastData[lbound].date);
+              $('p', this).text(podcastData[lbound].description);
+              $('.photo img', this).addClass('img-circle');
+              $('.photo img', this).attr('src',podcastData[lbound].image);
+              $('.photo img', this).attr('alt',podcastData[lbound].alt);
+              $('.photo a', this).attr('href',podcastData[lbound].audiofile);
+              $('.photo a', this).show();
+              lbound +=1;
+            }
+
+          });
+
+          showMore(); //run show more to limit characters in description
+        }
+    });
 
     //populate elements with podcast guests
     $.each($('#podcast .guest'),function(index, el) {
@@ -58,38 +90,6 @@ $(function(){
     });
 
     showMore(); //run show more to limit characters in description
-
-    //onclick to replace elements with new guests
-    $('.next-page button').click(function(){
-      var ubound = ($(this).val() * 9);
-      var lbound = (($(this).val() * 9) - 9);
-
-      $.each($('#podcast .guest'),function() {
-        if(podcastData[lbound] == null) {
-          $('h3', this).text('');
-          $('h4', this).text('');
-          $('p', this).text('');
-          $('.photo img', this).removeClass('img-circle');
-          $('.photo img', this).removeAttr('src');
-          $('.photo img', this).removeAttr('alt');
-          $('.photo a', this).hide();
-          lbound +=1;
-        } else {
-          $('h3', this).text(podcastData[lbound].guest);
-          $('h4', this).text('Episode #' + podcastData[lbound].episode + ' - ' + podcastData[lbound].date);
-          $('p', this).text(podcastData[lbound].description);
-          $('.photo img', this).addClass('img-circle');
-          $('.photo img', this).attr('src',podcastData[lbound].image);
-          $('.photo img', this).attr('alt',podcastData[lbound].alt);
-          $('.photo a', this).attr('href',podcastData[lbound].audiofile);
-          $('.photo a', this).show();
-          lbound +=1;
-        }
-
-      });
-
-      showMore(); //run show more to limit characters in description
-    });
 
     //search function
     $('.searchpodcast').click(function(){
@@ -105,38 +105,7 @@ $(function(){
 
 })
   .done(function(data) {
-      // var showChar = 100;  // How many characters are shown by default
-      // var ellipsestext = "...";
-      // var moretext = "Show more >>";
-      // var lesstext = "<< Show less";
-      //
-      //
-      // $('.guest-description').each(function() {
-      //   var content = $(this).html();
-      //
-      //   if(content.length > showChar) {
-      //
-      //       var c = content.substr(0, showChar);
-      //       var h = content.substr(showChar, content.length - showChar);
-      //
-      //       var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
-      //
-      //       $(this).html(html);
-      //   }
-      // });
-      //
-      // $(".morelink").click(function(){
-      //   if($(this).hasClass("less")) {
-      //       $(this).removeClass("less");
-      //       $(this).html(moretext);
-      //   } else {
-      //       $(this).addClass("less");
-      //       $(this).html(lesstext);
-      //   }
-      //   $(this).parent().prev().toggle();
-      //   $(this).prev().toggle();
-      //   return false;
-      // });
+
   })
   .fail(function() {
 
